6e6ee4f7444da2a675a57c7d8c8a104f60058c90
Elad Katz <eladk@seladom.sela.co.il>
2017-06-28 15:44:05 +0300

    End of 03

--- OperasWebsites/Global.asax.cs
+++ OperasWebsites/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using OperasWebsites.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,6 +18,9 @@ namespace OperasWebsites
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            Database.SetInitializer<OperasDB>(new OperasInitializer());
+
         }
     }
 }
